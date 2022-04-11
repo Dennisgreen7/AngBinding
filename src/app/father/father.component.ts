1a0871234b9864e8c755cb2e68780cd2d2535f3c
Dennisgreen7 <73803629+Dennisgreen7@users.noreply.github.com>
2022-04-11 16:26:42 +0300

    Update father.component.ts

--- src/app/father/father.component.ts
+++ src/app/father/father.component.ts
@@ -15,9 +15,9 @@ export class FatherComponent implements OnInit {
   showCar: boolean = false;
   chosenCar: Car = {carName:'',img:''};
   cars: Car []= [
-    {carName:'Ferrari',img:'dist/binding-hw/assets/images/Ferrar.jpg'},
-    {carName:'Lamborghini',img:'dist/binding-hw/assets/images/Lamborghini.jpg'},
-    {carName:'Mercedes-Benz',img:'dist/binding-hw/assets/images/MercedesBenz.jpg'}
+    {carName:'Ferrari',img:'docs/assets/images/Ferrar.jpg'},
+    {carName:'Lamborghini',img:'docs/assets/images/Lamborghini.jpg'},
+    {carName:'Mercedes-Benz',img:'docs/assets/images/MercedesBenz.jpg'}
   ];
   getCar(car: string) {
     if (car === "Select") {
